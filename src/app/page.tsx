e4463b9ac55a1f091a75df3ce110a30f479b1a7c
oyeon-kwon <ohyeon.kwon@flask.global>
2024-09-06 14:33:27 +0900

    fix build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
+"use client";
+
 import styles from "./page.module.css";
-import Editor from "./editor";
+
+import dynamic from "next/dynamic";
+
+const Editor = dynamic(
+  () => {
+    return import("./editor");
+  },
+  { ssr: false }
+);
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <Editor />
-    </div>
-  );
+  return <div className={styles.page}>{<Editor />}</div>;
 }
